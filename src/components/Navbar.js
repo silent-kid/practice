eea49fda67e3b7a45192e1696139aab1fe84a4d1
silent-kid <jawwadm0000@gmail.com>
2024-11-17 19:34:44 +0530

    first sommit

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../style/Navbar.css'
+
+const Navbar = ({videoRating}) => {
+  return (
+    <div className='nav'>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        Video Rating = {videoRating}
+    </div>
+  )
+}
+
+export default Navbar
